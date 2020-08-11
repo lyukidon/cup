fd2e5d91f0c5456fa81b1186f336ab81427f47d0
lyukidon <64675543+lyukidon@users.noreply.github.com>
2020-08-11 23:54:11 +0900

    Changelog
    1. 물을 채우고 나서 컵을 없앴다가 다시 생성하면 물이 찬 상태로 생기는 부분 안채워진 상태로 바뀌도록 수정
    
    2. water 버튼이 4번 이상 실행이 되지 않던 부분 수정

--- cup.js
+++ cup.js
@@ -1,5 +1,5 @@
 let can_u_see_the_cup=false;
-
+let count=0;
 let plate_arr=[document.getElementById('one'),document.getElementById('two'),document.getElementById('three'),document.getElementById('four')];
 
 function making_cup(){
@@ -9,10 +9,14 @@ function making_cup(){
         }
         document.getElementById('one').classList.add('cup_bottom');
         can_u_see_the_cup=true;
+        if(count!==0){
+            count=0;
+        }
     }
     else{
         for (let i=0;i<plate_arr.length;i++){
             plate_arr[i].classList.remove('cup');
+            plate_arr[i].style.backgroundColor ='white'
         }
         document.getElementById('one').classList.remove('cup_bottom');
         can_u_see_the_cup=false;
@@ -20,7 +24,7 @@ function making_cup(){
 }
 document.getElementById('cup_appear_btn').addEventListener('click',making_cup);
 
-let count=0;
+
 function pouring_water(){
     if (count<4){
         if (can_u_see_the_cup===true){
